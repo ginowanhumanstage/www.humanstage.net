4faadcc9e27d0523657976e2090c690d30c917ae
hirofumi_i <detroit69city.nau@gmail.com>
2019-07-20 14:17:24 +0900

    Fix path to liveItem component

--- src/components/liveItems.tsx
+++ src/components/liveItems.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
-import LiveItem from './LiveItem';
+import LiveItem from './liveItem';
 
 // interface IProps {
 //   data: Event[];
