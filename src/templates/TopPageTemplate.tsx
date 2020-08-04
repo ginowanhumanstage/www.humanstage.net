b3947a4a02d8c1a99336c6b28345990cfc760a12
hirofumi_i <detroit69city.nau@gmail.com>
2020-08-05 01:00:09 +0900

    Remove unnecessary code and image on the top page

--- src/templates/TopPageTemplate.tsx
+++ src/templates/TopPageTemplate.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link, graphql } from 'gatsby';
 import Helmet from 'react-helmet';
-import LiveItems from '../components/liveItems';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 import baseImg from '../images/img-base.jpg';
 
-
-export default ({ data, pageContext }) => {
+export default ({ pageContext }) => {
   return (
     <Layout lastSchedule={pageContext.lastSchedule}>
       <SEO title="" />
@@ -39,77 +36,6 @@ export default ({ data, pageContext }) => {
     </Layout>
   );
 };
-
-export const query = graphql`
-  query($gte: Date!) {
-    allWordpressPost(
-      sort: { fields: [slug], order: ASC }
-      filter: { slug: { gte: $gte } }
-      limit: 4
-    ) {
-      edges {
-        node {
-          id
-          title
-          featured_media {
-            localFile {
-              childImageSharp {
-                fluid(maxWidth: 1000, maxHeight: 620, cropFocus: CENTER) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-          slug
-          acf {
-            date
-            open
-            start
-            adv
-            act
-          }
-        }
-      }
-    }
-  }
-`;
-
-const Headline = styled.h1`
-  text-align: center;
-  font-size: 1.4rem;
-  font-weight: 400;
-  font-feature-settings: 'palt';
-
-  @media (min-width: 768px) {
-    font-size: 1.7rem;
-  }
-`;
-
-const LiveItemsWrapper = styled.div`
-  @media (min-width: 768px) {
-    margin: 0 auto 1rem;
-    width: calc(100% - 100px);
-  }
-`;
-
-const ButtonWrapper = styled.div`
-  text-align: center;
-`;
-
-const Button = styled.div`
-  display: inline-block;
-  border: 1px solid #ccc;
-  font-size: 1rem;
-  text-decoration: none;
-  color: #188fd9;
-  padding: 0.75rem 2rem;
-  background-color: #fff;
-  border-radius: 50px;
-
-  &:hover {
-    border-color: #ccc;
-  }
-`;
 
 const BaseLink = styled.p`
 　text-align: center;
