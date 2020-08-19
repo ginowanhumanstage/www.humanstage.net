b4893791602164f72873ec21d8812d1a8f094e9f
hirofumii <detroit69city.nau@gmail.com>
2020-08-19 16:27:17 +0100

    Fix environment variables so that the can be used in Client-side JavaScript

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: `https://${process.env.WP_BASE_URL}/graphql`,
+  uri: `https://${process.env.GATSBY_WP_BASE_URL}/graphql`,
   credentials: 'include',
 });
 
